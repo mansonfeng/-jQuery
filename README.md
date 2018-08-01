46b6218df283bf34dfc1bd86b60e001f472c865e
pink <pink>
2018-08-01 15:51:17 +0800

    last commit

---
+++ README.md
@@ -0,0 +1,32 @@
+项目描述
+此项目是一个响应式pc项目
+主要有首页，服务商，商家详情页
+采用模块化、组件化、工程化的模式开发
+
+
+技术选型  jQuery
+         express
+         art-template
+         ES6
+
+index.html  首页   header         service.html 服务商  header          serviceIn.html 商家详情页    header
+                  carousel                            scontent                                    mcontent
+                  content                              xxxx                                       xxxxx
+                  supply
+                  service
+                  footer
+                  Partner
+                  baian
+
+实现功能
+                   头部动画
+                   假轮播图
+                   分页
+
+
+
+
+
+
+
+
--- router/router.js
+++ router/router.js
@@ -4,6 +4,7 @@ var router  = express.Router();
 var index = require('../data/index.json');
 var service= require('../data/service.json');
 var item = require('../data/item.json');
+var comments= require('../data/comment.json')
 
 router.get('/index',function(req,res){
     res.send(index)
@@ -18,17 +19,19 @@ router.get('/item',function(req,res){
 })
 
 router.get('/comment',function (req,res) {
-    if (req.query.page){
+    res.send(comments)
+  /*  if (req.query.page){
         var page = req.query.page/1;
         var comment = comments.slice((page)*10,10*(page+1))
         res.send(comment)
     } else {
         var arr = []
-        for (var i = 1; i<=comments.length/10; i++){
+        for (var i=1; i<= comments.length/10; i++){
             arr.push(i)
         }
         res.send(arr)
-    }
+    }*/
 })
+
 module.exports = router;
 
